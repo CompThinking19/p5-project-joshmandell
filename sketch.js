8b6edda4a8ad18d6064783a4c65d536839b84a9a
joshmandell <55148072+joshmandell@users.noreply.github.com>
2019-12-15 19:42:49 -0500

    final commit

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
+//sets up the canvas
 let rectWidth;
+//sets up the asnwer array of the letters that spell out digital
 var answerArray = ["d", "i", "g", "i", "t", "a", "l"];
+//helps set up the scoring
 var haswon = false
 var correctX = 10
 var score =0
@@ -8,14 +11,14 @@ function setup() {
 	noStroke();
 	background(230);
 	rectWidth = width / 4;
-	// put setup code here
-}
 
+}
+// sets up the main draw function that everything works through
 function draw() {
 if (haswon == false) {
-
+//asks the user to put input
   var guess = prompt("Guess a letter");
-
+//uses the scoring functions
 		if (answerArray.includes(guess)) {
       text(guess, correctX , 30);
       correctX += 10
@@ -24,33 +27,18 @@ if (haswon == false) {
         if (score =7)  {prompt("Good Job! The asnwer is " + answerArray)}
   	}
 }
-		function keyPressed() {
-			let keyIndex = -1;
-			if (key >= 'z') {
-				keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
-			}
-			if (keyIndex === -1) {
-				for (var i = 0; i < word.length; i++) {
-					answerArray[i] = "_";
-				}
-				var remainingLetters = word.length;
-				while (remainingLetters > 0) {
-					alert(answerArray.join(" "));
-				}
-				if (guess === null) {} else if (guess.length !== 1) {
-					alert("please enter a single letter.");
-				} else {
-					for (var j = 0; j < word.length; j++) {
-						if (word[j] === guess) {
-							answerArray[j] = guess;
-							remainingLetters--;
-						}
-					}
-				}
-			}
-		}
+//from p5 examples i used this to setup the function that will accept user input by pressing keys
+function keyPressed() {
+  let keyIndex = -1;
+  if (key >= 'z') {
+    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
   }
+
+}
+}
+		//README This game is a hangman game that asks the user to guess the letters to the word "digital".
+    //I liked the thought of creating a hangman game and with the project using p5 i needed user input and this was a great way of doing so.
+    //Trying to make this was difficult I had to look up some ways to begin. I had a great deal of help from my instructor who helped me get rid of errors and help all my functions actually work.
 
 		// alert(answerArray.join(""));
 		 //alert("Good Job! The asnwer is " + answerArray);
-     //var userword = "" create array then check if letter user inputs is in array  if it is then delete from array  once array is empty then win
